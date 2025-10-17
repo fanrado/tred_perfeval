df91aa2bbda6ee1519e03ccad8a4ae425f98bc9b
fanrado <radofana@gmail.com>
2025-10-17 05:39:42 -0700

    read .npz file output of ACC_EFFQ

--- src/acc_effq_eval.py
+++ src/acc_effq_eval.py
@@ -1,4 +1,12 @@
-import os, sys
+import os, sys, json
+import numpy as np
+
+def load_npz(input_file=''):
+	data = np.load(input_file)
+	hits_keys = [k for k in data.keys() if ('hits' in k) and ('location' not in k)]
+	for k in hits_keys:
+		print(f'data[{k}] : {data[k]}')
 
 if __name__=='__main__':
-	pass
+	path_to_file = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ', '10x10_partitions.npz'])
+	load_npz(path_to_file)
