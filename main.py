962eb15aedc8798d02e546ac6f18a58feca796d9
fanrado <radofana@gmail.com>
2025-10-20 08:43:47 -0700

    generating the distribution of the deltaQ wrt to the reference (2,2,2) x (10,10)

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import os, sys, json
 # import matplotlib.pyplot as plt
 import src.peakmem_eval as peakmem_eval
 import src.runtime_eval as runtime_eval
-
+import src.acc_effq_eval as acc_effq_eval
 # input_dir = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/MEMORY_EVAL/no_dynBatchChunk/benchmark_plot'
-input_dir = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/MEMORY_EVAL/with_dynBatchChunk/benchmark_mem'
-# input_subdir = 'runon_wcgpu1'
-input_subdir = ''
-filename = 'peak_memory_usage_withdynamic_batching_chunking.json'
-input_file = '/'.join([input_dir, input_subdir, filename])
+
 
 def load_json(input_file=''):
 	with open(input_file, 'r') as f:
@@ -17,18 +13,40 @@ def load_json(input_file=''):
 	return data
 
 if __name__=='__main__':
+	# # Peak memory usage ----
 	# peakmem_eval.benchmark_peak_memory_nodynamic_chunking_and_batching(input_path=input_dir)
-	peakmem_eval.benchmark_peak_memory_dynamic_chunking_and_batching(input_path=input_dir)
-	# print(input_dir)
-	# data = load_json(input_file=input_file)
-	# organized_data = peakmem_eval.organize_peakmem_data(data=data)
-	# output_file = '/'.join([input_dir, input_subdir, 'peakmem_vs_Nsegments.png'])
-	# peakmem_eval.overlay_plots((organized_data['N_segments'], organized_data['peak_memory_perbatch'], f'Batch size: {organized_data['batch_size']}, nbchunk: {organized_data['nbchunk']}, nbchunk_conv: {organized_data['nbchunk_conv']}', 'red'), title='Peak memory vs N_segments', output_file=output_file, xlabel='N_segments', ylabel='Peak Memory (MB)')
-	# organized_data = runtime_eval.get_runtime_majorOperations(data=data)
-	# print(organized_data.keys())
-	# runtime_perbatch = runtime_eval.get_runtime_perbatch(data=data)
-	# runtime_eval.overlay_plots(((runtime_perbatch['N_segments'], runtime_perbatch['runtimes_perbatch'], f'Batch size: {runtime_perbatch["batch_size"]}, nbchunk: {runtime_perbatch["nbchunk"]}, nbchunk_conv: {runtime_perbatch["nbchunk_conv"]}', 'blue'),), title='Runtime vs N_segments', xlabel='N_segments', ylabel='Runtime (sec)', output_file='/'.join([input_dir, input_subdir, 'runtime_vs_Nsegments.png']))
-
+	# peakmem_eval.benchmark_peak_memory_dynamic_chunking_and_batching(input_path=input_dir)
+	# # --------------------------
+	# ## Runtime evaluation ----
+	# ## PIECHART RUNTIME for one event
+	# input_dir = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/RUNTIME_EVAL/benchmark_plot/to_use'
+	# # input_subdir = 'runon_wcgpu1'
+	# input_subdir = ''
+	# filename = 'runtime_batchsize8192_NBCHUNK100_NBCHUNKCONV50.json'
+	# input_file = '/'.join([input_dir, input_subdir, filename])
 	# data = load_json(input_file=input_file)
 	# runtime_majorOps = runtime_eval.get_runtime_majorOperations(data=data)
 	# runtime_eval.runtimeshare_majorOp(organized_data=runtime_majorOps, output_file='/'.join([input_dir, input_subdir, 'runtime_share_majorOps.png']))
+	# ## Overlay of the runtime for different batch schemes
+	# input_dir = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/RUNTIME_EVAL/benchmark_plot/to_use'
+	# runtime_eval.benchmark_runtime(input_path=input_dir)
+	# ## --------------------------
+
+	## Accuracy of the effective charge calculation ----
+	root_path = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/benchmark_plots'
+	path_to_ref = '/'.join([root_path, '10x10_partitions_2x2x2.npz'])
+	path_to_10x10_4x4x2 = '/'.join([root_path, '10x10_partitions_4x4x2.npz'])
+	path_to_8x8_2x2x2 = '/'.join([root_path, '8x8_partitions_2x2x2.npz'])
+	path_to_6x6_2x2x2 = '/'.join([root_path, '6x6_partitions_2x2x2.npz'])
+
+	deltaQ_10x10_4x4x2 = acc_effq_eval.get_deltaQ(npz_data=path_to_10x10_4x4x2, npz_ref=path_to_ref)
+	deltaQ_8x8_2x2x2 = acc_effq_eval.get_deltaQ(npz_data=path_to_8x8_2x2x2, npz_ref=path_to_ref)
+	deltaQ_6x6_2x2x2 = acc_effq_eval.get_deltaQ(npz_data=path_to_6x6_2x2x2, npz_ref=path_to_ref)
+
+	delta_Q_list = [(deltaQ_10x10_4x4x2, '(4,4,2) x (10,10)', 'red'),
+					(deltaQ_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
+					(deltaQ_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
+	# delta_Q_list = [(deltaQ_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
+	# 				(deltaQ_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
+	acc_effq_eval.overlay_hists_deltaQ(*delta_Q_list, title='Delta Q Distributions wrt (2,2,2) x (10,10)', xlabel='Delta Q [ke-]', ylabel='Counts', output_file='/'.join([root_path, 'deltaQ_overlay_10x10_8x8_6x6.png']))
+	## --------------------------
--- src/acc_effq_eval.py
+++ src/acc_effq_eval.py
@@ -7,7 +7,7 @@ def load_npz(input_file=''):
 	data = np.load(input_file)
 	hits_keys = [k for k in data.keys() if ('hits' in k) and ('location' not in k)]
 	hits = np.array([], dtype=np.float32)
-	print(hits_keys)
+	# print(hits_keys)
 	for k in hits_keys:
 		hits = rfn.append_fields(hits, k, data[k][:, -1], usemask=False)
 	return hits
@@ -28,30 +28,76 @@ def get_deltaQ(npz_data, npz_ref):
 		N = len(data[field])
 		if N > len(ref_data[field]):
 			N = len(ref_data[field])
-		try:
-			deltaQ = np.concatenate((deltaQ, data[field][:N] - ref_data[field][:N]))
-		except:
-			print(field)
-			print(data[field].shape, ref_data[field].shape)
+		# print('data[field][:N] = ', data[field][:N])
+		# sys.exit()
+		# try:
+		deltaQ = np.concatenate((deltaQ, data[field][:N] - ref_data[field][:N]))
+		# except:
+		# 	print(field)
+		# 	print(data[field].shape, ref_data[field].shape)
 	return deltaQ
 
+def overlay_hists_deltaQ(*deltaQ_list, title, xlabel, ylabel, output_file=''):
+	"""
+		Overlay the distributions of the deltaQ = data - ref.
+		Args:
+		    *deltaQ_list: a list of tuples, each tuple contains (deltaQ_array, label, color)
+			title (str): Title of the plot.
+			xlabel (str): Label for the x-axis.
+			ylabel (str): Label for the y-axis.
+			output_file (str): Filename to save the plot.
+	"""
+	plt.figure(figsize=(10, 6))
+	for (deltaQ, label, color) in deltaQ_list:
+		plt.hist(deltaQ, bins=100, histtype='step', color=color, label=label)
+		# plt.hist(deltaQ, bins=100, range=(-1000, 1000), histtype='step', color=color, label=label)
+	plt.xlabel(xlabel, fontsize=18)
+	plt.ylabel(ylabel, fontsize=18)
+	plt.yscale('log')
+	plt.xticks(fontsize=18)
+	plt.yticks(fontsize=18)
+	plt.title(title, fontsize=18)
+	plt.tight_layout()
+	plt.legend(fontsize=18)
+	plt.grid(True)
+	plt.savefig(output_file)
+	plt.close()
+
 if __name__=='__main__':
-	path_to_ref = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/node_2x2x2', '10x10_partitions.npz'])
-	path_to_data = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ', '10x10_partitions_afterconversion.npz'])
-	# path_to_6x6 = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ', '6x6_partitions.npz'])
+	# path_to_ref = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/node_2x2x2', '10x10_partitions.npz'])
+	# path_to_data = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ', '10x10_partitions_afterconversion.npz'])
+	# # path_to_6x6 = '/'.join(['/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ', '6x6_partitions.npz'])
+	# # deltaQ = get_deltaQ(npz_data=path_to_data, npz_ref=path_to_ref)
 	# deltaQ = get_deltaQ(npz_data=path_to_data, npz_ref=path_to_ref)
-	deltaQ = get_deltaQ(npz_data=path_to_data, npz_ref=path_to_ref)
-	# deltaQ_6x6 = get_deltaQ(npz_data=path_to_6x6, npz_ref=path_to_ref)
-	# print(deltaQ)
-	# k = load_npz(input_file=path_to_data)
+	# # deltaQ_6x6 = get_deltaQ(npz_data=path_to_6x6, npz_ref=path_to_ref)
+	# # print(deltaQ)
+	# # k = load_npz(input_file=path_to_data)
+	# plt.figure(figsize=(10,6))
+	# plt.hist(deltaQ, bins=100, range=(-1000, 1000), histtype='step', color='blue', label='deltaQ (10x10_changeinconfig - 10x10_changeinpy)')
+	# plt.xlabel('Delta Q [ke-]')
+	# plt.ylabel('Counts')
+	# plt.yscale('log')
+	# plt.title('Delta Q Distribution between 10x10_after and 10x10_before')
+	# plt.legend()
+	# plt.grid()
+	# plt.savefig('deltaQ_10x10_vs_10x10.png')
+	# plt.close()
+	root_path = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/benchmark_plots'
+	path_to_ref = '/'.join([root_path, '10x10_partitions_2x2x2.npz'])
+	path_to_10x10_4x4x2 = '/'.join([root_path, '10x10_partitions_4x4x2.npz'])
+	path_to_8x8_2x2x2 = '/'.join([root_path, '8x8_partitions_2x2x2.npz'])
+	path_to_6x6_2x2x2 = '/'.join([root_path, '6x6_partitions_2x2x2.npz'])
+
+	deltaQ_10x10_4x4x2 = get_deltaQ(npz_data=path_to_10x10_4x4x2, npz_ref=path_to_ref)
+	deltaQ_8x8_2x2x2 = get_deltaQ(npz_data=path_to_8x8_2x2x2, npz_ref=path_to_ref)
 	plt.figure(figsize=(10,6))
-	plt.hist(deltaQ, bins=100, range=(-1000, 1000), histtype='step', color='blue', label='deltaQ (10x10_changeinconfig - 10x10_changeinpy)')
+	plt.hist(deltaQ_10x10_4x4x2, bins=100, range=(-1000, 1000), histtype='step', color='blue', label='deltaQ 810x10 4x4x2')
+	plt.hist(deltaQ_8x8_2x2x2, bins=100, range=(-1000, 1000), histtype='step', color='red', label='deltaQ 8x8 2x2x2')
 	plt.xlabel('Delta Q [ke-]')
 	plt.ylabel('Counts')
 	plt.yscale('log')
 	plt.title('Delta Q Distribution between 10x10_after and 10x10_before')
 	plt.legend()
 	plt.grid()
-	plt.savefig('deltaQ_10x10_vs_10x10.png')
+	plt.savefig('test.png')
 	plt.close()
-	
