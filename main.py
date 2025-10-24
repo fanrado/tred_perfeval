801f2f2ebf4ae50ed55d40a09d73484a205c3186
fanrado <radofana@gmail.com>
2025-10-23 22:35:24 -0400

    change related to acc_effq: save Q and Qref in a hdf5, then overlay the distributions

--- main.py
+++ main.py
@@ -40,38 +40,50 @@ if __name__=='__main__':
 	deltaQ_8x8_2x2x2, dQ_over_Q_8x8_2x2x2 = None, None
 	deltaQ_6x6_2x2x2, dQ_over_Q_6x6_2x2x2 = None, None
 	readFrom_npz = False
+	cut_on_Q = 1 # ke-
 	if readFrom_npz:
 		path_to_ref = '/'.join([root_path, '10x10_partitions_2x2x2.npz'])
 		path_to_10x10_4x4x2 = '/'.join([root_path, '10x10_partitions_4x4x2.npz'])
 		path_to_8x8_2x2x2 = '/'.join([root_path, '8x8_partitions_2x2x2.npz'])
 		path_to_6x6_2x2x2 = '/'.join([root_path, '6x6_partitions_2x2x2.npz'])
 
-		output_file_10x10_hdf5 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_10x10_4x4x2.hdf5'])
-		output_file_8x8_hdf5 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_8x8_2x2x2.hdf5'])
-		output_file_6x6_hdf5 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_6x6_2x2x2.hdf5'])
+		output_file_10x10_hdf5 = '/'.join([root_path, 'HDF5/accumulatedcharge_10x10_4x4x2.hdf5'])
+		output_file_8x8_hdf5 = '/'.join([root_path, 'HDF5/accumulatedcharge_8x8_2x2x2.hdf5'])
+		output_file_6x6_hdf5 = '/'.join([root_path, 'HDF5/accumulatedcharge_6x6_2x2x2.hdf5'])
 		saveHDF5 = True
-		deltaQ_10x10_4x4x2, dQ_over_Q_10x10_4x4x2 = acc_effq_eval.get_deltaQ_fromNPZ(npz_data=path_to_10x10_4x4x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_10x10_hdf5)
-		deltaQ_8x8_2x2x2, dQ_over_Q_8x8_2x2x2 = acc_effq_eval.get_deltaQ_fromNPZ(npz_data=path_to_8x8_2x2x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_8x8_hdf5)
-		deltaQ_6x6_2x2x2, dQ_over_Q_6x6_2x2x2 = acc_effq_eval.get_deltaQ_fromNPZ(npz_data=path_to_6x6_2x2x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_6x6_hdf5)
+		acc_effq_eval.npz2hdf5(npz_data=path_to_10x10_4x4x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_10x10_hdf5)
+		acc_effq_eval.npz2hdf5(npz_data=path_to_8x8_2x2x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_8x8_hdf5)
+		acc_effq_eval.npz2hdf5(npz_data=path_to_6x6_2x2x2, npz_ref=path_to_ref, saveHDF5=saveHDF5, output_hdf5=output_file_6x6_hdf5)
 	else:
-		hdf5_file_10x10 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_10x10_4x4x2.hdf5'])
-		hdf5_file_8x8 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_8x8_2x2x2.hdf5'])
-		hdf5_file_6x6 = '/'.join([root_path, 'HDF5/deltaQ_dQoverQ_6x6_2x2x2.hdf5'])
-		deltaQ_10x10_4x4x2, dQ_over_Q_10x10_4x4x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_10x10)
-		deltaQ_8x8_2x2x2, dQ_over_Q_8x8_2x2x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_8x8)
-		deltaQ_6x6_2x2x2, dQ_over_Q_6x6_2x2x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_6x6)
+		hdf5_file_10x10 = '/'.join([root_path, 'HDF5/accumulatedcharge_10x10_4x4x2.hdf5'])
+		hdf5_file_8x8 = '/'.join([root_path, 'HDF5/accumulatedcharge_8x8_2x2x2.hdf5'])
+		hdf5_file_6x6 = '/'.join([root_path, 'HDF5/accumulatedcharge_6x6_2x2x2.hdf5'])
+		# deltaQ_10x10_4x4x2, dQ_over_Q_10x10_4x4x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_10x10)
+		# deltaQ_8x8_2x2x2, dQ_over_Q_8x8_2x2x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_8x8)
+		# deltaQ_6x6_2x2x2, dQ_over_Q_6x6_2x2x2 = acc_effq_eval.load_deltaQ_fromHDF5(hdf5_file=hdf5_file_6x6)
+		deltaQ_10x10_4x4x2, dQ_over_Q_10x10_4x4x2, Npix_tot_10x10, Npix_belowthr_10x10 = acc_effq_eval.load_Q_fromHDF5(hdf5_file=hdf5_file_10x10, cut_on_Qref=cut_on_Q)
+		deltaQ_8x8_2x2x2, dQ_over_Q_8x8_2x2x2, Npix_tot_8x8, Npix_belowthr_8x8 = acc_effq_eval.load_Q_fromHDF5(hdf5_file=hdf5_file_8x8, cut_on_Qref=cut_on_Q)
+		deltaQ_6x6_2x2x2, dQ_over_Q_6x6_2x2x2, Npix_tot_6x6, Npix_belowthr_6x6 = acc_effq_eval.load_Q_fromHDF5(hdf5_file=hdf5_file_6x6, cut_on_Qref=cut_on_Q)
+		print(f'10x10_4x4x2: Total pixels considered = {Npix_tot_10x10}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_10x10}')
+		print(f'8x8_2x2x2: Total pixels considered = {Npix_tot_8x8}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_8x8}')
+		print(f'6x6_2x2x2: Total pixels considered = {Npix_tot_6x6}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_6x6}')
 
-	delta_Q_list = [(deltaQ_10x10_4x4x2, '(4,4,2) x (10,10)', 'red'),
-					(deltaQ_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
-					(deltaQ_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
-	output_file = '/'.join([root_path, 'deltaQ_overlay_10x10_8x8_6x6.png'])
-	# output_file = '/'.join([root_path, 'test.png'])
-	acc_effq_eval.overlay_hists_deltaQ(*delta_Q_list, title='Accumulated charge distributions wrt (2,2,2) x (10,10)', xlabel='Delta Q [ke-]', ylabel='Counts', output_file=output_file)
+		delta_Q_list = [(deltaQ_10x10_4x4x2, '(4,4,2) x (10,10)', 'red'),
+						(deltaQ_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
+						(deltaQ_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
+		output_file = '/'.join([root_path, 'deltaQ_overlay_10x10_8x8_6x6.png'])
+		# output_file = '/'.join([root_path, 'test.png'])
+		acc_effq_eval.overlay_hists_deltaQ(*delta_Q_list, title='Accumulated charge distributions wrt (2,2,2) x (10,10)', xlabel='Delta Q [ke-]', ylabel='Counts', output_file=output_file)
 
-	dQ_over_Q_list = [(dQ_over_Q_10x10_4x4x2, '(4,4,2) x (10,10)', 'red'),
-					(dQ_over_Q_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
-					(dQ_over_Q_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
-	output_file = '/'.join([root_path, 'dQ_over_Q_overlay_10x10_8x8_6x6.png'])
-	# output_file = '/'.join([root_path, 'test.png'])
-	acc_effq_eval.overlay_hists_deltaQ(*dQ_over_Q_list, title='Relative difference of the charges at each pixel', xlabel=r'$(Q-Q_{ref})/Q_{ref}$', ylabel='Counts', output_file=output_file, cut=1e3)
-	## --------------------------
+		dQ_over_Q_list = [(dQ_over_Q_10x10_4x4x2, '(4,4,2) x (10,10)', 'red'),
+						(dQ_over_Q_8x8_2x2x2, '(2,2,2) x (8,8)', 'green'),
+						(dQ_over_Q_6x6_2x2x2, '(2,2,2) x (6,6)', 'blue')]
+		output_file = '/'.join([root_path, 'dQ_over_Q_overlay_10x10_8x8_6x6.png'])
+		# output_file = '/'.join([root_path, 'test.png'])
+		acc_effq_eval.overlay_hists_deltaQ(*dQ_over_Q_list, title='Relative difference of the charges at each pixel', xlabel=r'$(Q-Q_{ref})/Q_{ref}$', ylabel='Counts', output_file=output_file)
+		with open('/'.join([root_path, 'accuracy_summary.txt']), 'w') as f:
+			f.write(f'Accuracy evaluation summary (pixels with Q_ref < {cut_on_Q} ke- are excluded):\n')
+			f.write(f'10x10_4x4x2: Total pixels considered = {Npix_tot_10x10}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_10x10}\n')
+			f.write(f'8x8_2x2x2: Total pixels considered = {Npix_tot_8x8}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_8x8}\n')
+			f.write(f'6x6_2x2x2: Total pixels considered = {Npix_tot_6x6}, pixels below Q_ref threshold of {cut_on_Q} ke- = {Npix_belowthr_6x6}\n')
+		## --------------------------
