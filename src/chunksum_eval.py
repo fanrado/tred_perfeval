3e5e6bba570593189845a83ea39a1592c7c5f7f9
fanrado <radofana@gmail.com>
2025-11-14 07:21:19 -0800

    Refactor code. Separating the plotting function

--- src/chunksum_eval.py
+++ src/chunksum_eval.py
@@ -6,48 +6,8 @@ import mplhep as hep
 def load_json(fname):
 	with open(fname, 'r') as f:
 		return json.load(f)
-# def get_chunksum_runtime(data_json: dict):
-# 	"""
-# 	Extract the runtime information from the chunksum evaluation JSON data_json.
-# 	Args:
-# 		data_json (dict): The JSON data containing chunksum evaluation results.
-# 	Returns:
-# 		dict: A dictionary with chunk sizes as keys and their corresponding runtimes.
-# 	"""
-# 	chunksum_readout_shape = ''
-# 	chunksum_i_shape = ''
-# 	chunksum_qblock_shape = ''
-# 	chunksum_shape = {
-# 		'chunksum_readout_shape': '',
-# 		'chunksum_i_shape': '',
-# 		'chunksum_qblock_shape': ''
-# 	}
-# 	for itpc, tpc_data in data_json.items():
-# 		print(itpc)
-# 		if not isinstance(tpc_data, dict):
-# 			print(f'Not a dict : {itpc}')
-# 			if 'chunksum' in itpc:
-# 				chunksum_shape[itpc] = tpc_data
-# 			continue
-# 		for key, val in tpc_data.items():
-# 			print(f'key : {key}')
-# 			continue
-# 			if not isinstance(val, dict):
-# 				continue
-# 			for key1, val1 in val.items():
-# 				print(f'key1 : {key1}')
-# 				continue
-# 				if not isinstance(val1, dict):
-# 					continue
-# 				for key2, val2 in val1.items():
-# 					## Batch level
-# 					# print(f'key2 : {key2}')
-# 					if not isinstance(val2, dict):
-# 						continue
-# 					for key3, val3 in val2.items():
-# 						print(f'key3 : {key3}')
 
-def get_chunksum_runtime(data_json: dict):
+def get_chunksum_runtime(data_json: dict, output_path: str='../tests'):
 	organized_data = {}
 	batch_size = data_json['batch_size']
 	nbchunk = data_json['nbchunk']
@@ -109,6 +69,32 @@ def get_chunksum_runtime(data_json: dict):
 					t_chunksum_qblock_perbatch += tmp_chunksum_qblock_sec
 				runtime_chunksum_qblock_sec.append(t_chunksum_qblock_perbatch)
 				runtime_chunksum_i_sec.append(t_chunksum_i_perbatch)
+	## plot distributions of the runtimes
+	output_dist = f'{output_path}/dist_runtime_chunksum/{chunksum_readout_shape}_{chunksum_qblock_shape}_{chunksum_i_shape}_dist.png'
+	plot_dist = True
+	if plot_dist:
+		plt.figure(figsize=(15, 5))
+		plt.subplot(1, 3, 1)
+		plt.hist(runtime_chunksum_readout_sec, bins=30, color='blue', alpha=0.7)
+		plt.xlabel('Runtime Chunksum Readout (sec)')
+		plt.ylabel('#')
+		plt.title('Distribution of Chunksum Readout Runtime')
+		#
+		plt.subplot(1, 3, 2)
+		plt.hist(runtime_chunksum_qblock_sec, bins=30, color='green', alpha=0.7)
+		plt.xlabel('Runtime Chunksum Qblock (sec)')
+		plt.ylabel('#')
+		plt.title('Distribution of Chunksum Qblock Runtime')
+		#
+		plt.subplot(1, 3, 3)
+		plt.hist(runtime_chunksum_i_sec, bins=30, color='red', alpha=0.7)
+		plt.xlabel('Runtime Chunksum current (sec)')
+		plt.ylabel('#')
+		plt.title('Distribution of Chunksum current Runtime')
+		#
+		plt.tight_layout()
+		plt.savefig(output_dist)
+		plt.close()
 	Runtime_summary = {
 		'Nbin_chunksum_readout': int(chunksum_readout_shape.split('x')[-1]),
 		'Nbin_chunksum_qblock': int(chunksum_qblock_shape.split('x')[-1]),
@@ -122,13 +108,29 @@ def get_chunksum_runtime(data_json: dict):
 	}
 	return Runtime_summary
 
+def plot_chunksum_runtime_vs_Nbins(Nbins: list, t_mean: list, t_std: list, output_path: str, xlabel: str='Number of bins', ylabel: str='Runtime (sec)', title: str='Chunksum Runtime vs Number of bins', filename: str='chunksum_runtime_vs_Nbins.png'):
+	plt.figure(figsize=(10, 8))
+	hep.style.use("CMS") 
+	plt.errorbar(Nbins, t_mean, yerr=t_std, fmt='o--', ecolor='r', capsize=5, label=r'Mean $\pm stdev$')
+	# plt.plot(Nbins_sorted, t_mean_sorted, '*--', label=r'Mean $\pm stdev$')
+	plt.xlabel(xlabel, fontsize=20)
+	plt.ylabel(ylabel, fontsize=20)
+	plt.title(title, fontsize=20)
+	plt.xticks(fontsize=15)
+	plt.yticks(fontsize=15)
+	plt.legend(fontsize=15)
+	plt.grid(True)
+	plt.tight_layout()
+	plt.savefig(f'{output_path}/{filename}')
+	plt.close()
+
 def get_runtime_chunksum_qblock(path_to_file: str, output_path: str):
 	list_json = [f for f in os.listdir(path_to_file) if f.endswith('.json')]
 	Nbins 	= []
 	t_mean 	= []
 	t_std 	= []
 	for f in list_json:
-		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)))
+		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)), output_path=output_path)
 		if runtime['Nbin_chunksum_qblock'] not in Nbins:
 			Nbins.append(runtime['Nbin_chunksum_qblock'])
 			t_mean.append(runtime['runtime_chunksum_qblock_sec_mean'])
@@ -138,19 +140,17 @@ def get_runtime_chunksum_qblock(path_to_file: str, output_path: str):
 	Nbins_sorted = [Nbins_dict[i] for i in sorted_indices]
 	t_mean_sorted = [t_mean[i] for i in sorted_indices]
 	t_std_sorted = [t_std[i] for i in sorted_indices]
-	plt.figure(figsize=(10, 8))
-	hep.style.use("CMS") 
-	plt.errorbar(Nbins_sorted, t_mean_sorted, yerr=t_std_sorted, fmt='o', ecolor='r', capsize=5, label=r'Mean $\pm stdev$')
-	plt.xlabel('Number of bins in chunksum_qblock', fontsize=20)
-	plt.ylabel('Runtime (sec)', fontsize=20)
-	plt.title('Chunksum_qblock Runtime vs Number of bins', fontsize=20)
-	plt.xticks(fontsize=15)
-	plt.yticks(fontsize=15)
-	plt.legend(fontsize=15)
-	plt.grid(True)
-	plt.tight_layout()
-	plt.savefig(f'{output_path}/chunksum_qblock_runtime_vs_Nbins.png')
-	plt.close()
+
+	# dump data to a json file
+	output_data = {
+		'Nbins': Nbins_sorted,
+		't_mean': t_mean_sorted,
+		't_std': t_std_sorted
+	}
+	with open(f'{output_path}/chunksum_qblock_runtime_vs_Nbins.json', 'w') as f:
+		json.dump(output_data, f)
+	# plot
+	plot_chunksum_runtime_vs_Nbins(Nbins=Nbins_sorted, t_mean=t_mean_sorted, t_std=t_std_sorted, output_path=output_path, xlabel='Number of bins in chunksum_qblock', ylabel='Runtime (sec)', title='Chunksum_qblock Runtime vs Number of bins', filename='chunksum_qblock_runtime_vs_Nbins.png')
 
 def get_runtime_chunksum_i(path_to_file: str, output_path: str):
 	list_json = [f for f in os.listdir(path_to_file) if f.endswith('.json')]
@@ -158,9 +158,12 @@ def get_runtime_chunksum_i(path_to_file: str, output_path: str):
 	t_mean 	= []
 	t_std 	= []
 	for f in list_json:
-		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)))
+		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)), output_path=output_path)
+		# print(runtime)
 		if runtime['Nbin_chunksum_i'] not in Nbins:
-			print(runtime['Nbin_chunksum_i'], runtime['runtime_chunksum_i_sec_mean'])
+			if runtime['Nbin_chunksum_i'] < 32:
+				continue
+			# print(runtime['Nbin_chunksum_i'], runtime['runtime_chunksum_i_sec_mean'])
 			Nbins.append(runtime['Nbin_chunksum_i'])
 			t_mean.append(runtime['runtime_chunksum_i_sec_mean'])
 			t_std.append(runtime['runtime_chunksum_i_sec_std'])
@@ -169,19 +172,17 @@ def get_runtime_chunksum_i(path_to_file: str, output_path: str):
 	Nbins_sorted = [Nbins_dict[i] for i in sorted_indices]
 	t_mean_sorted = [t_mean[i] for i in sorted_indices]
 	t_std_sorted = [t_std[i] for i in sorted_indices]
-	plt.figure(figsize=(10, 8))
-	hep.style.use("CMS") 
-	plt.errorbar(Nbins_sorted, t_mean_sorted, yerr=t_std_sorted, fmt='o', ecolor='r', capsize=5, label=r'Mean $\pm stdev$')
-	plt.xlabel('Number of bins in chunksum_current', fontsize=20)
-	plt.ylabel('Runtime (sec)', fontsize=20)
-	plt.title('Chunksum_current Runtime vs Number of bins', fontsize=20)
-	plt.xticks(fontsize=15)
-	plt.yticks(fontsize=15)
-	plt.legend(fontsize=15)
-	plt.grid(True)
-	plt.tight_layout()
-	plt.savefig(f'{output_path}/chunksum_current_runtime_vs_Nbins.png')
-	plt.close()
+
+	# dump data in a json file
+	data_dict = {
+		'Nbins': Nbins_sorted,
+		't_mean': t_mean_sorted,
+		't_std': t_std_sorted
+	}
+	with open(f'{output_path}/chunksum_i_runtime_vs_Nbins.json', 'w') as f:
+		json.dump(data_dict, f)
+	#
+	plot_chunksum_runtime_vs_Nbins(Nbins=Nbins_sorted, t_mean=t_mean_sorted, t_std=t_std_sorted, output_path=output_path, xlabel='Number of bins in chunksum_current', ylabel='Runtime (sec)', title='Chunksum current Runtime vs Number of bins', filename='chunksum_current_runtime_vs_Nbins.png')
 
 def get_runtime_chunksum_readout(path_to_file: str, output_path: str):
 	list_json = [f for f in os.listdir(path_to_file) if f.endswith('.json')]
@@ -189,7 +190,7 @@ def get_runtime_chunksum_readout(path_to_file: str, output_path: str):
 	t_mean 	= []
 	t_std 	= []
 	for f in list_json:
-		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)))
+		runtime = get_chunksum_runtime(data_json=load_json(os.path.join(path_to_file, f)), output_path=output_path)
 		if runtime['Nbin_chunksum_readout'] not in Nbins:
 			print(runtime['Nbin_chunksum_readout'], runtime['runtime_chunksum_readout_sec_mean'])
 			Nbins.append(runtime['Nbin_chunksum_readout'])
@@ -200,22 +201,20 @@ def get_runtime_chunksum_readout(path_to_file: str, output_path: str):
 	Nbins_sorted = [Nbins_dict[i] for i in sorted_indices]
 	t_mean_sorted = [t_mean[i] for i in sorted_indices]
 	t_std_sorted = [t_std[i] for i in sorted_indices]
-	plt.figure(figsize=(10, 8))
-	hep.style.use("CMS") 
-	plt.errorbar(Nbins_sorted, t_mean_sorted, yerr=t_std_sorted, fmt='o', ecolor='r', capsize=5, label=r'Mean $\pm stdev$')
-	plt.xlabel('Number of bins in chunksum_readout', fontsize=20)
-	plt.ylabel('Runtime (sec)', fontsize=20)
-	plt.title('Chunksum_readout Runtime vs Number of bins', fontsize=20)
-	plt.xticks(fontsize=15)
-	plt.yticks(fontsize=15)
-	plt.legend(fontsize=15)
-	plt.grid(True)
-	plt.tight_layout()
-	plt.savefig(f'{output_path}/chunksum_readout_runtime_vs_Nbins.png')
-	plt.close()
+	# dump data in a json file
+	data_dict = {
+		'Nbins': Nbins_sorted,
+		't_mean': t_mean_sorted,
+		't_std': t_std_sorted
+	}
+	with open(f'{output_path}/chunksum_readout_runtime_vs_Nbins.json', 'w') as f:
+		json.dump(data_dict, f)
+	# plot
+	plot_chunksum_runtime_vs_Nbins(Nbins=Nbins_sorted, t_mean=t_mean_sorted, t_std=t_std_sorted, output_path=output_path, xlabel='Number of bins in chunksum_readout', ylabel='Runtime (sec)', title='Chunksum_readout Runtime vs Number of bins', filename='chunksum_readout_runtime_vs_Nbins.png')
+
 
 if __name__=='__main__':
-	path_to_file = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/CHUNKSUM_EVAL'
+	path_to_file = '/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/CHUNKSUM_EVAL/combined'
 	# list_json = [f for f in os.listdir(path_to_file) if f.endswith('.json')]
 	# ##
 	# ## chunksum current
